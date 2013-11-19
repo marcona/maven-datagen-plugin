ae97865684aba10e8164adb32d80a6a80d082342
Administrator <admin@codjo.net>
2013-11-19 18:58:22 +0100

    Fix Unit test issue: target repository is no more ignored in data directory

---
+++ src/test/data/compileClient/target/src/client-badjava/java/com/MyGui.java
@@ -0,0 +1,6 @@
+package com;
+public class MyGui {
+    public static void main(String[] args){
+      je ne compile pas
+    }
+}
---
+++ src/test/data/compileServer/target/src/server/java/MyHandler.java
@@ -0,0 +1,5 @@
+public class MyHandler {
+    public static void main(String[] args){
+      System.out.println("Je suis un Handler");
+    }
+}
